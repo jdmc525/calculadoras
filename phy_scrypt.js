ab04b0094726633f8297bc98609f56cf56e8262b
jdmc525 <121909687+jdmc525@users.noreply.github.com>
2024-01-19 19:38:55 -0600

    Terminada por completo funcionalidad de Fisica. Queda pendiente animaciones y mejorar la datalist

--- phy_scrypt.js
+++ phy_scrypt.js
@@ -10,34 +10,49 @@ let phy_typeSelector = document.querySelector('#phy_operation-opt');
 let phy_optionsForm = document.querySelector('#phy-main_options-form');
 let phy_optionsBtn  = document.querySelector('#phy-options_ok');
 
-let phyPOS_OpExtCont =document.querySelector('.phy-pos_external-container');
+let phyPOS_OpExtCont =document.querySelector('.phy-op_external-container-POS');
+let phyVEL_OpExtCont =document.querySelector('.phy-vel_ext-cont-main');
+let phyACE_OpExtCont =document.querySelector('.phy-ace_ext-cont-main');
 
 
 let mtrcSelection = "" ;
+let mtrcSelectionVel = "" ;
+let mtrcSelectionAcc = "" ;
 
 phy_optionsForm.addEventListener('submit', function(event){
     event.preventDefault()
     switch (phy_typeSelector.value) {
         case "Posicion":
             phyPOS_OpExtCont.classList.remove('hide');
+            phyVEL_OpExtCont.classList.add('hide');
+            phyACE_OpExtCont.classList.add('hide');
         break;
         
         case "Velocidad":
-            phyPOS_OpExtCont.classList.remove('hide');
+            phyVEL_OpExtCont.classList.remove('hide');
+            phyPOS_OpExtCont.classList.add('hide');
+            phyACE_OpExtCont.classList.add('hide');
         break;
 
         case "Aceleracion":
-            phyPOS_OpExtCont.classList.remove('hide');
+            phyACE_OpExtCont.classList.remove('hide');
+            phyPOS_OpExtCont.classList.add('hide');
+            phyVEL_OpExtCont.classList.add('hide');
         break;
 
         default:
+            alert('Opcion no valida. Porfavor elija una de las opciones establecidas');
         break;
     }
 
     if (phy_mtrcSelector.value == "Ingles") {
-        mtrcSelection = "pie/s";
+        mtrcSelectionPos = "pie";
+        mtrcSelectionVel = "pie/s";
+        mtrcSelectionAce = "pie/s^2"
     }else if(phy_mtrcSelector.value == "Internacional"){
-        mtrcSelection = "m/s";
+        mtrcSelectionPos = "m";
+        mtrcSelectionVel = "m/s";
+        mtrcSelectionAce = "m/s^2"
     }
 
 
@@ -63,14 +78,14 @@ op_PosF.addEventListener('submit', function(event){
     let time = parseFloat(posF_TIME.value);
     let result = (posIn) + (velIn)*(time) + ((0.5)*(ace)*(time)*(time));
 
-    posF_result.innerText = result + " "+ mtrcSelection;
+    posF_result.innerText = result + " "+ mtrcSelectionPos;
 
     posF_result.classList.remove('hide');
 });
 
 
 
-/*---------OPERACION POSICION FINAL---------*/
+/*---------OPERACION POSICION INICIAL---------*/
 
 let posI_PosFin = document.querySelector('#posI_POS-FIN');
 let posI_VelIn = document.querySelector('#posI_VEL-I');
@@ -88,10 +103,75 @@ op_PosI.addEventListener('submit', function(event){
     let time = parseFloat(posI_TIME.value);
     let result = (posFin) - (velIn)*(time) - ((0.5)*(ace)*(time)*(time));
 
-    posI_result.innerText = result + " "+ mtrcSelection;
+    posI_result.innerText = result + " "+ mtrcSelectionPos;
 
     posI_result.classList.remove('hide');
 });
 
 
 
+/*---------OPERACION VELOCIDAD FINAL---------*/
+
+let velF_VelIn = document.querySelector('#velF_VEL-I');
+let velF_ACE  = document.querySelector('#velF_ACE');
+let velF_TIME = document.querySelector('#velF_TIME');
+
+let velF_result = document.querySelector('.velF-result');
+let op_velF = document.querySelector('#operation-velF');
+
+op_velF.addEventListener('submit', function(event){
+    event.preventDefault();
+    let velIn = parseFloat(velF_VelIn.value);
+    let ace = parseFloat(velF_ACE.value);
+    let time = parseFloat(velF_TIME.value);
+    let result =  (velIn) + ((ace)*(time));
+
+    velF_result.innerText = result + " "+ mtrcSelectionVel;
+
+    velF_result.classList.remove('hide');
+});
+
+
+/*---------OPERACION VELOCIDAD INICIAL---------*/
+
+let velI_VelFin = document.querySelector('#velI_VEL-F');
+let velI_ACE  = document.querySelector('#velI_ACE');
+let velI_TIME = document.querySelector('#velI_TIME');
+
+let velI_result = document.querySelector('.velI-result');
+let op_velI = document.querySelector('#operation-velI');
+
+op_velI.addEventListener('submit', function(event){
+    event.preventDefault();
+    let velFin = parseFloat(velI_VelFin.value);
+    let ace = parseFloat(velI_ACE.value);
+    let time = parseFloat(velI_TIME.value);
+    let result =  (velFin) - ((ace)*(time));
+
+    velI_result.innerText = result + " "+ mtrcSelectionVel;
+
+    velI_result.classList.remove('hide');
+});
+
+
+
+/*---------OPERACION VELOCIDAD INICIAL---------*/
+
+let ace_VelFin = document.querySelector('#ace_VEL-F');
+let ace_VelIn  = document.querySelector('#ace_VEL-I');
+let ace_TIME = document.querySelector('#ace_TIME');
+
+let ace_result = document.querySelector('.ace-result');
+let op_Ace = document.querySelector('#operation-ace');
+
+op_Ace.addEventListener('submit', function(event){
+    event.preventDefault();
+    let velFin = parseFloat(ace_VelFin.value);
+    let velIn = parseFloat(ace_VelIn.value);
+    let time = parseFloat(ace_TIME.value);
+    let result =  ((velFin) - (velIn)) / (time);
+
+    ace_result.innerText = result + " "+ mtrcSelectionAce;
+
+    ace_result.classList.remove('hide');
+});
